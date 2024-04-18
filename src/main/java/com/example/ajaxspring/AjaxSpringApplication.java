22aeb4dd5d19931327cefe5c6e5d7783206d273a
wild-mental <wild.mental@kidsnote.com>
2024-04-19 04:47:32 +0900

    initial commit

---
+++ src/main/java/com/example/ajaxspring/AjaxSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajaxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ajaxspring/AjaxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajaxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
